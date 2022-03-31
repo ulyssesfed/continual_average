6bef26fa949640b6cced931e53955682c5a64d78
gf211097 <ulyfleen@gmail.com>
2022-03-31 12:41:50 +0100

    fixed

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,8 +5,7 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        // Write a program to calculate and print average (or mean) of the stream of given numbers.
-        // write the code here
+
         Scanner scanner = new Scanner(System.in);
         int sum = 0;
         int count = 0;
